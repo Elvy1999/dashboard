e6ec060b0b1d4691a5dd04cee131ba64d6f794ef
elvydecoo <elvydecoo99@gmail.com>
2023-09-12 17:53:38 -0400

    Added event listeners to the project cards so when they are clicked they open a new tab with the selected project

---
+++ app.js
@@ -0,0 +1,22 @@
+const calculator_card = document.getElementById("calculator");
+const landing_page_card = document.getElementById("landing_page");
+const summary_card = document.getElementById("summary");
+const etchASketch_card = document.getElementById("EtchASketch");
+const rockPaperScissors_card = document.getElementById("rockPaperScissors");
+
+calculator_card.addEventListener("click", () => {
+  window.open("https://elvy1999.github.io/calculator/", "_blank");
+});
+landing_page_card.addEventListener("click", () => {
+  window.open("https://elvy1999.github.io/landing_page1/", "_blank");
+});
+summary_card.addEventListener("click", () => {
+  window.open("https://elvy1999.github.io/results_summary/", "_blank");
+});
+etchASketch_card.addEventListener("click", () => {
+  window.open("https://elvy1999.github.io/Etch-a-sketch/", "_blank");
+});
+
+rockPaperScissors_card.addEventListener("click", () => {
+  window.open("https://elvy1999.github.io/RockPaperScissor/", "_blank");
+});
